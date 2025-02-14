c4021af1dd16a227fac0d92cfe05944b86e63ed3
LotekingDev <Jeehgamerwinchester@gmail.com>
2025-02-14 10:47:02 -0300

    3 tentativas de login
    adicionado a função de 3 tentativas login ,caso as tentativas forem incorretas o programa fecha automaticamente

--- Cartorio_De_aluno_EBAC.c
+++ Cartorio_De_aluno_EBAC.c
@@ -50,7 +50,7 @@ int registrar()// Função responsavel por cadastrar os usuários no sitema
 	fprintf(file,",");
 	fclose(file);
 	
-	printf("Digite o cargo a ser cadastrado; ");
+	printf("Digite o cargo a ser cadastrado: ");
 	scanf("%s",cargo);//amazenando a variavel "cargo" ultilizando  a Função string "%s"
 	
 	file = fopen(arquivo, "a");
@@ -105,99 +105,122 @@ int consultar()
 	
 }
 
-int deletar()
+int deletar() //Função responsavel por deletar usuários
 {
 	char cpf[40];
+
+	printf("Digite o CPF do usuário a ser deletado: "); //Mensagem ao usuário
+	scanf("%s",cpf); // CPF e o ponto chave do nosso cartório 
+	 
+	FILE *file; //Cria o arquivo 
+	file = fopen(cpf,"r"); //Abre o arquivo, e o "r" é para ler o arquivo 
+	fclose(file); // Obs: fechar arquivos
 	
-	printf("Digite o CPF do usuário a ser deletado: ");
-	scanf("%s",cpf);
-	
-	remove(cpf);
-	
-	FILE *file;
-	file = fopen(cpf,"r");
-	
-	if(file == NULL)
+	if(file == NULL) // "E SE" o arquivo não for encontrado
 	{
-		printf("O usuário não se encontra no sistema.\n");
-		system("pause");
-		
+		printf("\nO usuário não se encontra no sistema!.\n"); //Mensagem ao usuário 
+		system("pause"); //Pausa o sistema até o usuário continuar 
 	}
+	
+	remove(cpf); // Opcao que apaga arquivos
+	
+	if(file != NULL) // != significa "se for diferente de a coloque b"
+	{
+		printf("\nO usuário foi deletado com sucesso!\n\n"); //Mensagem ao usuário
+		system("pause"); //Pausa o sitema até o usuário continuar 
+	}
+	
 }
 
 int main()
- {
-	int opcao=0;
-	int laco=1;
-	char login[40]="a";
-	char senha[10]="a";
-	int comparacaologin, comparacaosenha;
-	
-	setlocale(LC_ALL, "Portuguese");//Definição da linguagems
+{
+    int opcao = 0;
+    int laco = 1;
+    char login[40] = "a";
+    char senha[10] = "a";
+    int comparacaologin, comparacaosenha;
+    int tentativas = 0; // Variável para controlar o número de tentativas
 
-	printf("###Cartório da EBAC ###\n\n");
-	printf("login de adminstrador!\n\nLogin: "); // Solicita o login (nome de usuário)
-	scanf("%s",login);
-	
-	
-	printf("Senha: "); // Solicita a senha
-	scanf("%s",senha);
-	
-	comparacaologin = strcmp(login, "ebac");// Compara login
-	
-	comparacaosenha = strcmp(senha, "admin");// Compara senha
-	
-	if(comparacaologin == 0 && comparacaosenha == 0)
-	{
-	
-	
-	   for(laco=1;laco=1;)//repetição do sistema 
-	 { 
-	
-	    system("cls");
-	   
-	    setlocale(LC_ALL, "Portuguese");//Definição da linguagems
-	
-	    printf("###Cartório De Alunos Da EBAC###\n\n");
-	    printf("Escolha As Opções\n\n");//entrada para o menu
-   	    printf("\t1 - Registra Um Novo Aluno\n");
-	    printf("\t2 - Consutar Nome Do Aluno\n");
-	    printf("\t3 - Deletar Um Aluno\n\n");
-	    printf("\t4 - Sair do Sistema\n\n");
-	    printf("Opçâo: ");//Fim do menu
-	
-	    scanf("%d", &opcao);//amazenando a escolha do usuário
-	
-	    system("cls");//responsavel por limpar a tela
-	
-	    switch(opcao)//inicio de escolha do usuário
-	    {
-	  	  case 1:
-	  	  registrar();//chama de funções
-		  break;
-		
-		  case 2:
-		  consultar();
-		  break;
-		
-		  case 3:
-		  deletar();
-		  break;
-		
-		  case 4:
-		  printf("Obrigado por ultilizar o Sistema!\n");
-		  return 0;
-		  break;
-		
-		  default:
-		  printf("Essa Opçâo Não Está Disponivel!\n");
-		  system("pause");
-		  break;	
-	    }//fim da escolha do usuário
-     }
-   }
-  
-  else
-        printf("Senha incorreta!");
- }
+    setlocale(LC_ALL, "Portuguese"); // Definição da linguagem
+
+    printf("### Cartório da EBAC ###\n\n");
+
+    for (tentativas = 0; tentativas < 3; tentativas++)// Loop para permitir até 3 tentativas
+    {
+        printf("Login de administrador!\n\n");
+        printf("Login: "); // Solicita o login (nome de usuário)
+        scanf("%s", login);
+
+        printf("Senha: "); // Solicita a senha
+        scanf("%s", senha);
+
+        comparacaologin = strcmp(login, "ebac"); // Compara login
+        comparacaosenha = strcmp(senha, "admin"); // Compara senha
+
+        if (comparacaologin == 0 && comparacaosenha == 0)
+        {
+            // Se o login e senha estiverem corretos, entra no sistema
+            printf("Login bem-sucedido!\n");
+            break; // Sai do loop se o login for bem-sucedido
+        }
+        else
+        {
+            // Se as credenciais estiverem incorretas
+            printf("Login ou senha incorretos. ");
+            printf("Tentativas restantes: %d\n", 2 - tentativas); // Informa o número de tentativas restantes
+            if (tentativas == 2) // Se for a última tentativa
+            {
+                printf("Você excedeu o número de tentativas. O programa será encerrado.\n");
+                return 0; // Fecha o programa após 3 tentativas
+            }
+        }
+    }
+
+    // Caso o login tenha sido bem-sucedido, segue o resto do código
+    while (1) // Inicia o loop principal do sistema
+    {
+        system("cls");
+
+        setlocale(LC_ALL, "Portuguese"); // Definição da linguagem
+
+        printf("### Cartório De Alunos Da EBAC ###\n\n");
+        printf("Escolha as opções:\n\n"); // Entrada para o menu
+        printf("\t1 - Registrar um Novo Aluno\n");
+        printf("\t2 - Consultar Nome do Aluno\n");
+        printf("\t3 - Deletar um Aluno\n\n");
+        printf("\t4 - Sair do Sistema\n\n");
+        printf("Opção: "); // Fim do menu
+
+        scanf("%d", &opcao); // Armazena a escolha do usuário
+
+        system("cls"); // Responsável por limpar a tela
+
+        switch (opcao) // Início da escolha do usuário
+        {
+            case 1:
+                registrar(); // Chama a função registrar
+                break;
+
+            case 2:
+                consultar(); // Chama a função consultar
+                break;
+
+            case 3:
+                deletar(); // Chama a função deletar
+                break;
+
+            case 4:
+                printf("Obrigado por utilizar o Sistema!\n");
+                return 0; // Sai do programa
+                break;
+
+            default:
+                printf("Essa opção não está disponível!\n");
+                system("pause");
+                break;
+        }
+    }
+
+    return 0; // Finaliza o programa
+}
 
